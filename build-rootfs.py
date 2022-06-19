11ddf7a355f99f6ae611f67e371a8de9c95a546d
MaxPeal <30347730+MaxPeal@users.noreply.github.com>
2022-06-19 21:22:44 +0200

    Update build-rootfs.py

--- build-rootfs.py
+++ build-rootfs.py
@@ -1,12 +1,9 @@
+#!/bin/env python3
+
 import urllib.request
 import os
 import platform
 import subprocess
-
-os.makedirs('rootfs/bin')
-os.makedirs('rootfs/sbin')
-os.makedirs('rootfs/usr/bin')
-os.makedirs('rootfs/usr/sbin')
 
 print(platform.architecture())
 print(platform.machine())
@@ -16,7 +13,7 @@ print(platform.processor())
 print(platform.system())
 #print(platform.system_alias())
 print(platform.uname())
-print(platform.freedesktop_os_release())
+#print(platform.freedesktop_os_release())
 
 toybox_arch_mapping = {
     'aarch64': 'aarch64',
@@ -34,13 +31,21 @@ toybox_arch_mapping = {
     'x86_64': 'x86_64'
 }
 
-# print('http://landley.net/toybox/bin/toybox-{toybox_arch_mapping[platform.machine()]}')
+#print("foo",{toybox_arch_mapping[platform.machine()]})
+print("http://landley.net/toybox/bin/toybox-",{toybox_arch_mapping[platform.machine()]})
+
+os.makedirs('rootfs/bin')
+os.makedirs('rootfs/sbin')
+os.makedirs('rootfs/usr/bin')
+os.makedirs('rootfs/usr/sbin')
 
 with urllib.request.urlopen(f'http://landley.net/toybox/bin/toybox-{toybox_arch_mapping[platform.machine()]}') as f:
     # print(f)
     toybox = f.read()
     with open('rootfs/bin/toybox', 'wb') as file:
         file.write(toybox)
+        print(f.url)
+        print(f.status)
         os.chmod('rootfs/bin/toybox', 0o777)
     toybox_commands = subprocess.run(['rootfs/bin/toybox', '--long'], capture_output=True, text=True).stdout.split()
     print(toybox_commands)
